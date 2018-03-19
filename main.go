6da5ddf7ec597ff6ea1ee5588f53531948f63d0d
Utako Ueda <uueda@pivotal.io>
2018-03-19 15:42:01 -0700

    good app

--- main.go
+++ main.go
@@ -7,34 +7,13 @@ import (
 )
 
 func main() {
-	fmt.Print("Starting...\n")
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", good_app)
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
 	}
-	//
-	// s := &http.Server{
-	// 	Addr:           ":" + os.Getenv("PORT"),
-	// 	ReadTimeout:    10 * time.Second,
-	// 	WriteTimeout:   10 * time.Second,
-	// 	MaxHeaderBytes: 1 << 20,
-	// 	ConnState:      ConnStateListener,
-	// }
-	//
-	// panic(s.ListenAndServe())
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
+func good_app(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(res, "good app.")
 }
-
-// func ConnStateListener(c net.Conn, cs http.ConnState) {
-// 	if cs == http.StateNew {
-// 		fmt.Println("New connection! Closing.")
-// 		c.Close()
-// 	} else {
-// 		fmt.Printf("CONN STATE: %v, %v\n", cs, c)
-// 	}
-// 	fmt.Printf("CONN STATE: %v, %v\n", cs, c)
-// }
