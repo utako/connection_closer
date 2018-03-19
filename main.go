adb52845376175e9a8bcdeafa24d60f28e34e78e
Utako Ueda <uueda@pivotal.io>
2018-03-19 12:24:14 -0700

    add good app

--- main.go
+++ main.go
@@ -2,37 +2,39 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"net/http"
 	"os"
-	"time"
 )
 
 func main() {
 	fmt.Print("Starting...\n")
 	http.HandleFunc("/", hello)
-
-	s := &http.Server{
-		Addr:           ":" + os.Getenv("PORT"),
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-		ConnState:      ConnStateListener,
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
 	}
-
-	panic(s.ListenAndServe())
+	//
+	// s := &http.Server{
+	// 	Addr:           ":" + os.Getenv("PORT"),
+	// 	ReadTimeout:    10 * time.Second,
+	// 	WriteTimeout:   10 * time.Second,
+	// 	MaxHeaderBytes: 1 << 20,
+	// 	ConnState:      ConnStateListener,
+	// }
+	//
+	// panic(s.ListenAndServe())
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Printf("hello")
+	fmt.Fprintln(res, "good app.")
 }
 
-func ConnStateListener(c net.Conn, cs http.ConnState) {
-	if cs == http.StateNew {
-		fmt.Println("New connection! Closing.")
-		c.Close()
-	} else {
-		fmt.Printf("CONN STATE: %v, %v\n", cs, c)
-	}
-	fmt.Printf("CONN STATE: %v, %v\n", cs, c)
-}
+// func ConnStateListener(c net.Conn, cs http.ConnState) {
+// 	if cs == http.StateNew {
+// 		fmt.Println("New connection! Closing.")
+// 		c.Close()
+// 	} else {
+// 		fmt.Printf("CONN STATE: %v, %v\n", cs, c)
+// 	}
+// 	fmt.Printf("CONN STATE: %v, %v\n", cs, c)
+// }
